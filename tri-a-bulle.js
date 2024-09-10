1a426b5e19a6f54084089ab21462d55bfaea688a
yahya-Boussakla <yahyaboussakla@gmail.com>
2024-09-10 13:23:59 +0100

    tri bulle v2

--- tri-a-bulle.js
+++ tri-a-bulle.js
@@ -1,15 +1,19 @@
-let numbers = [4,3,2,5,8,0];
-
+let numbers = [1,2,3,4,5,6];
+let trie = true;
 let temps ;
 
 for (let i = 0; i < numbers.length; i++) {
 
     for (let j = 0; j < numbers.length - i - 1; j++) {
         if (numbers[j] > numbers[j+1]) {
+            trie = false;
             temps = numbers[j];
             numbers[j] = numbers[j+1];
             numbers[j+1] = temps;
         }
+    }
+    if (trie == true) {
+        return;
     }
 }
 console.log(numbers);
